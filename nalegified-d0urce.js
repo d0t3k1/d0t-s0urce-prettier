9e413030853df26b959e4d27b6b21e1599299577
Naleg <83455500+NalegFR@users.noreply.github.com>
2024-06-20 22:14:08 +0200

    forgor to change version

--- nalegified-d0urce.js
+++ nalegified-d0urce.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         nalegified-d0urce
-// @version      2024-06-19
+// @version      2024-06-20
 // @description  Get a prettier s0urce.io environment!
 // @author       Naleg
 // @originator   d0t & Xen0o2
